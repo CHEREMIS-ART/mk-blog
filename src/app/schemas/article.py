b738b103a1a9ac0704da355853f2c4cb2328a226
acheremisin <alexeycheremisin.pochta@gmail.com>
2025-12-18 10:42:24 +0300

    feat: add categoryand articles routers

---
+++ src/app/schemas/article.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class ArticleBase(BaseModel):
+    title: str
+    text: str
+    category_id: int
+
+
+class ArticleCreate(ArticleBase):
+    pass
+
+
+class ArticleUpdate(BaseModel):
+    title: Optional[str] = None
+    text: Optional[str] = None
+    category_id: Optional[int] = None
+
+
+class ArticleOut(BaseModel):
+    id: int
+    title: str
+    text: str
+    category_id: int
+    image_url: Optional[str] = None
+    author_id: int
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class PaginatedArticles(BaseModel):
+    items: list[ArticleOut]
+    page_number: int
+    page_size: int
+    total: int
+    total_pages: int
